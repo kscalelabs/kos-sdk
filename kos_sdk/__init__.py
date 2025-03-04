52789e6a78bb80fa65b25cabbe5f468638040aa8
Verda Korzeniewski <verdakor@gmail.com>
2025-03-03 23:47:37 -0800

    add version string/remove error

--- kos_sdk/__init__.py
+++ kos_sdk/__init__.py
@@ -2,4 +2,5 @@
 
 from kos_sdk.utils import Joint, JointGroup, JointState, Robot, RobotConfig, deg_to_rad, rad_to_deg
 
+__version__ = "0.1.0"
 __all__ = ["Joint", "JointGroup", "JointState", "Robot", "RobotConfig", "deg_to_rad", "rad_to_deg"]
